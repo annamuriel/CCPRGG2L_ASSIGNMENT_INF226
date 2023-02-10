a7580a2ec94983f78347eebf0e9f97ff99b7fad6
Anna Nicole Muriel <122036602+annamuriel@users.noreply.github.com>
2023-02-10 14:23:07 +0800

    Create App.java

---
+++ exercise5/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Person me = new Person();
+
+       // System.out.println(me.name);
+
+        // set method
+        me.setName("abcdef");
+
+        // get method
+        System.out.println(me.getName());
+
+        // Daughter object
+        Daughter myself = new Daughter();
+
+        System.out.println(myself.surname);
+
+        myself.showSurname();
+
+    }
+}
